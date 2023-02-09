5a13ceb54fe3413152747bf7e1b13283ffab59f6
GeorgioArmani <117245302+GeorgioArmani@users.noreply.github.com>
2023-02-09 17:25:10 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
-
+valid_email = 'egorkaivanov98@gmail.com'
+valid_password = 'RollsRoyce23'
+empty_email = ''
+empty_password = ''
+empty_auth_key = {'key': ''}
+incorrect_auth_key = {'key': '111111111111111111111111111111111111111111111'}
